1fe2a0447ef1f77ddab20d60ef922cfea9297a02
b0e2 <beenjeong02@gmail.com>
2026-07-28 18:34:32 +0900

    feat: docker-compose.yml 추가 (web 단일 서비스)

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 redis_host = os.environ.get("REDIS_HOST", "localhost")
-r = redis.Redis(host=redis_host, port=6379, decode_response=True)
+r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 
 
 @app.get("/")
